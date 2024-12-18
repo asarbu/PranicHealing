99ea2c267654ba297838ea1309b30bed56f6bc95
asarbu <uidj5379@contiwan.com>
2024-12-18 11:16:56 +0200

    Fixed HTML and JS for contact and events

---
+++ js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", async (event) => {
+    const now = new Date().toISOString();
+    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent('asociatiapranichealing@gmail.com')}/events?key=${encodeURIComponent('AIzaSyD0UZN5b9sPmNQlQ1lAtkShgJmJ5iRmRPY')}&timeMin=${encodeURIComponent(now)}`);
+    
+    if(!response.ok) throw Error('Nu se pot incarca evenimentele din calendar');
+
+    const calendar = await response.json();
+    /** @type {Array} */
+    const events = calendar.items;
+    events.forEach((event) => {
+        const eventList = document.getElementById('lista-evenimente');
+        const eventListItem = document.createElement('li');
+        const header = document.createElement('h5');
+        header.textContent = event.summary;
+
+        const dateOptions = {
+            dateStyle: 'long',
+            timeStyle: 'short',
+        };
+        const startDate = Date.parse(event.start.dateTime);
+        const endDate = Date.parse(event.end.dateTime)
+        const dateParagraph = document.createElement('p');
+        dateParagraph.textContent = Intl.DateTimeFormat('ro-RO', dateOptions).format(startDate) + ' - ' + Intl.DateTimeFormat('ro-RO', dateOptions).format(endDate);
+
+        const inscriere = document.createElement('div');
+        const anchor = document.createElement('a');
+        anchor.href = event.htmlLink;
+        anchor.textContent = 'Inscriere';
+
+        eventListItem.appendChild(header);
+        eventListItem.appendChild(dateParagraph);
+        if(event.description) {
+            var t = document.createElement('template');
+            t.innerHTML = event.description;
+            eventListItem.appendChild(t.content);
+        }
+        inscriere.appendChild(anchor);
+        eventListItem.appendChild(inscriere);
+        eventList.appendChild(eventListItem);
+    });
+
+    document.getElementById('wapp-button')?.addEventListener('click', () => {
+        const number = '40722348687';
+        const wappName = document.getElementById('wapp-name').value;
+        const wappNumber = document.getElementById('wapp-number').value;
+        const encodedMessage = encodeURIComponent(`Buna ziua! Ma numesc ${wappName} si as dori sa aflu mai mutle despre Pranic Healing. Va rog sa ma contactati la numarul de telefon ${wappNumber}`);
+        const url = `https://wa.me/${number}?text=${encodedMessage}`;
+        window.open(url, '_blank');
+    })
+});
